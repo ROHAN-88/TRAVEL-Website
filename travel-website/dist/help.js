14f294e9b577b45ffa0fc1ffa6b843841ddfedb3
Rohan <therohanshrestha6655@gmail.com>
2023-04-08 14:54:53 +0545

    change-1

---
+++ travel-website/dist/help.js
@@ -0,0 +1,29 @@
+// const myCarouselElement = document.querySelector('#carouselExampleIndicators')
+
+// const carousel = new bootstrap.Carousel(myCarouselElement, {
+//   interval: 2000,
+//   touch: true
+// })
+
+
+// const myCarouselel2 = document.querySelector('#carouselExampleIndicators-2');
+
+// const carousel2 = new bootstrap.Carousel(myCarouselel2, {
+//     interval: 2000,
+//     touch: false
+//   })
+
+
+
+  const form = document.querySelector('form');
+  const body = document.querySelector('body');
+
+  const sub = ()=>{
+
+          console.log("submit");
+
+       form.style.display="none";
+          
+            
+          // body.innerHTML="WE WILL GET BACK TO YOU";
+  };
